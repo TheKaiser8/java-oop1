d8a2a61b6334524665c1337c77452793b4fe8bbf
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-06 14:43:14 +0200

    Add print to console bank program

--- src/org/lessons/java/bank/Main.java
+++ src/org/lessons/java/bank/Main.java
@@ -9,6 +9,39 @@ public class Main {
         String name = scan.nextLine();
         Account account = new Account(name);
 
+        boolean exit = false;
+        do {
+            System.out.println("Che operazione vuoi fare?");
+            System.out.println("1-Versamento 2-Prelevamento 3-Esci");
+            String choice = scan.nextLine();
+            switch (choice) {
+                case "1":
+                    // Versamento
+                    System.out.print("Versamento");
+                    System.out.print("Cifra: ");
+                    double depositAmount = scan.nextDouble();
+                    account.deposit(depositAmount);
+                    System.out.println("Il tuo saldo: " + account.getBalance());
+                    break;
+                case "2":
+                    // Prelevamento
+                    System.out.println("Prelevamento");
+                    System.out.print("Cifra: ");
+                    double withdrawAmount = scan.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    System.out.println("Il tuo saldo: " + account.getBalance());
+                    break;
+                case "3":
+                    // Uscita
+                    System.out.println("Arrivederci!");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Scelta non valida");
+                    break;
+            }
+        } while (!exit);
+
         scan.close();
     }
 }
